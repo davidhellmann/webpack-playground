fac044f9da7c9b068d4c83411411a6347905950a
David Hellmann <davidhellmann.com@gmail.com>
2017-01-26 22:56:34 +0100

    Hm

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,30 @@
-var path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+const path = require('path');
 
 module.exports = {
-  entry: './app/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, '___dist')
-  }
+    entry: './___src/assets/js/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, '___dist/assets/js')
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015']
+                }
+            }
+        ]
+    },
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin(),
+        new HtmlWebpackPlugin({
+                template: './___src/index.html'
+            }
+        )
+    ]
 };
